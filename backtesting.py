2c65f00b05d927c3008d99291ee9819f2da8da80
Donovan Lock <donovanlock@gmail.com>
2025-08-17 21:41:42 +0100

    Replaced single-pair trading with multi-pair

--- backtesting.py
+++ backtesting.py
@@ -1,3 +1,4 @@
+import sys
 import pandas as pd
 
 from config import ANNUAL_TRADING_DAYS, PORTFOLIO_PORTION_INVESTED_PER_TRADE, RISK_FREE_RATE, STARTING_CAPITAL
@@ -13,14 +14,6 @@ def get_invested_capital(long_stock, short_stock, long_shares,
     short_market_value = ((2 * short_entry_price) - short_stock) * short_shares
     return long_market_value + short_market_value
 
-def update_capital(idle_capital, long_stock, short_stock,
-                   long_shares, short_shares, short_entry_price):
-    return idle_capital + get_invested_capital(long_stock, short_stock, long_shares,
-                                               short_shares, short_entry_price)
-
-def reset_to_flat_position(current_capital):
-    return 0, 0, 0, current_capital
-
 def enter_position(investment_sum, dependent_stock,
                    independent_stock, hedge_ratio, signal):
     if signal == Signal.ENTER_LONG or signal == Signal.EXIT_SHORT_AND_ENTER_LONG:
@@ -33,87 +26,93 @@ def enter_position(investment_sum, dependent_stock,
         short_entry_price = dependent_stock
     return long_shares, short_shares, short_entry_price
 
-
-def get_capital(dependent_stock, independent_stock,
-                positions, signals, hedge_ratio):
-    signal_entries = signals.dropna()
-    capital = pd.Series(index=signal_entries.index)
-    invested_capital = pd.Series(index=signal_entries.index)
-    current_capital = STARTING_CAPITAL # current_capital = idle_capital + long_market_value + short_market_value
-    long_shares, short_shares, short_entry_price, idle_capital = reset_to_flat_position(current_capital)
-
-    for i in signal_entries.index:
-        position = Position[positions[i]]
-        signal = Signal[signals[i]]
-
-        if signal == Signal.NONE:
-            if position == Position.LONG:
-                #update current_capital
-                current_capital = update_capital(idle_capital, dependent_stock[i], independent_stock[i],
-                                                 long_shares, short_shares, short_entry_price)
-            elif position == Position.SHORT:
-                #update current_capital
-                current_capital = update_capital(idle_capital, independent_stock[i], dependent_stock[i],
-                                                 long_shares, short_shares, short_entry_price)
-        elif signal == Signal.ENTER_LONG:
-            #enter long
-            investment_sum = PORTFOLIO_PORTION_INVESTED_PER_TRADE * current_capital
-            long_shares, short_shares, short_entry_price = enter_position(investment_sum, dependent_stock[i],
-                                                                          independent_stock[i], hedge_ratio, signal)
-            idle_capital = current_capital - get_invested_capital(dependent_stock[i], independent_stock[i],
-                                                                  long_shares, short_shares, short_entry_price)
-        elif signal == Signal.ENTER_SHORT:
-            #enter short
-            investment_sum = PORTFOLIO_PORTION_INVESTED_PER_TRADE * current_capital
-            long_shares, short_shares, short_entry_price = enter_position(investment_sum, dependent_stock[i],
-                                                                          independent_stock[i], hedge_ratio, signal)
-            idle_capital = current_capital - get_invested_capital(independent_stock[i], dependent_stock[i],
-                                                                  long_shares, short_shares, short_entry_price)
-        elif signal == Signal.EXIT_LONG:
-            #update current_capital and exit long
-            current_capital = update_capital(idle_capital, dependent_stock[i], independent_stock[i],
-                                             long_shares, short_shares, short_entry_price)
-            long_shares = short_shares = short_entry_price = 0
-            idle_capital = current_capital
-        elif signal == Signal.EXIT_SHORT:
-            #update current_capital and exit short
-            current_capital = update_capital(idle_capital, independent_stock[i], dependent_stock[i],
-                                             long_shares, short_shares, short_entry_price)
-            long_shares, short_shares, short_entry_price, idle_capital = reset_to_flat_position(current_capital)
-        elif signal == Signal.EXIT_LONG_AND_ENTER_SHORT:
-            #update current_capital and exit long
-            #enter short
-            current_capital = update_capital(idle_capital, dependent_stock[i], independent_stock[i],
-                                             long_shares, short_shares, short_entry_price)
-            investment_sum = PORTFOLIO_PORTION_INVESTED_PER_TRADE * current_capital
-            long_shares, short_shares, short_entry_price = enter_position(investment_sum, dependent_stock[i],
-                                                                          independent_stock[i], hedge_ratio, signal)
-            idle_capital = current_capital - get_invested_capital(independent_stock[i], dependent_stock[i],
-                                                                  long_shares, short_shares, short_entry_price)
-        else:
-            #update current_capital and exit short
-            #enter long
-            current_capital = update_capital(idle_capital, independent_stock[i], dependent_stock[i],
-                                             long_shares, short_shares, short_entry_price)
-            investment_sum = PORTFOLIO_PORTION_INVESTED_PER_TRADE * current_capital
-            long_shares, short_shares, short_entry_price = enter_position(investment_sum, dependent_stock[i],
-                                                                          independent_stock[i], hedge_ratio, signal)
-            idle_capital = current_capital - get_invested_capital(dependent_stock[i], independent_stock[i],
-                                                                  long_shares, short_shares, short_entry_price)
-        
-        capital[i] = current_capital
-        invested_capital[i] = current_capital - idle_capital
+def test(pairs):
+    indexCheck = all(pair.data['Signal'].index.equals(pairs[0].data['Signal'].index) for pair in pairs[1:])
+    if not indexCheck:
+        sys.exit("All spreads must have the same index for backtesting.") #this should never happen
     
-    return capital, invested_capital
+    for pair in pairs:
+        pair.data['Invested'] = pd.Series(index=pair.data['Signal'].dropna().index, data=0)
 
-def get_sharpe_ratio(invested_capital):
-    invested_returns = invested_capital.pct_change().dropna()
-    valid_returns = invested_returns[(invested_capital.shift(1) > 0) & (invested_capital > 0)]
-    risk_free_rate = RISK_FREE_RATE / ANNUAL_TRADING_DAYS
-    mean_return = valid_returns.mean()
-    stdev_return = valid_returns.std()
-    sharpe_ratio = (mean_return - risk_free_rate) / stdev_return
-    return sharpe_ratio
+    capital = pd.Series(index=pairs[0].data['Signal'].dropna().index)
+    uninvested_capital = STARTING_CAPITAL
+    for i in capital.index:
+        # update invested capital for each pair
+        # calculate total capital
+        for pair in pairs:
+            position = Position[pair.data['Position'][i]]
+            signal = Signal[pair.data['Signal'][i]]
+            dependent_stock_price = pair.data[pair.dependent_stock][i]
+            independent_stock_price = pair.data[pair.independent_stock][i]
 
-def get_roi(ending_capital):
-    return (ending_capital - STARTING_CAPITAL) / STARTING_CAPITAL * 100
+            if signal == Signal.NONE:
+                if position == Position.LONG:
+                    pair.invested_capital = get_invested_capital(dependent_stock_price,
+                                                                 independent_stock_price,
+                                                                 pair.long_shares, pair.short_shares,
+                                                                 pair.short_entry_price)
+                elif position == Position.SHORT:
+                    pair.invested_capital = get_invested_capital(independent_stock_price,
+                                                                 dependent_stock_price,
+                                                                 pair.long_shares, pair.short_shares,
+                                                                 pair.short_entry_price)
+
+            elif signal == Signal.ENTER_LONG:
+                investment_sum = PORTFOLIO_PORTION_INVESTED_PER_TRADE * uninvested_capital
+                pair.long_shares, pair.short_shares, pair.short_entry_price = enter_position(investment_sum, dependent_stock_price,
+                                                                          independent_stock_price, pair.hedge_ratio, signal)
+                pair.invested_capital = investment_sum
+                uninvested_capital -= investment_sum
+            
+            elif signal == Signal.ENTER_SHORT:
+                investment_sum = PORTFOLIO_PORTION_INVESTED_PER_TRADE * uninvested_capital
+                pair.long_shares, pair.short_shares, pair.short_entry_price = enter_position(investment_sum, dependent_stock_price,
+                                                                          independent_stock_price, pair.hedge_ratio, signal)
+                pair.invested_capital = investment_sum
+                uninvested_capital -= investment_sum
+            
+            elif signal == Signal.EXIT_LONG:
+                pair.invested_capital = get_invested_capital(dependent_stock_price,
+                                                             independent_stock_price,
+                                                             pair.long_shares, pair.short_shares,
+                                                             pair.short_entry_price)
+                uninvested_capital += pair.invested_capital
+                pair.invested_capital = pair.long_shares = pair.short_shares = pair.short_entry_price = 0
+                
+            elif signal == Signal.EXIT_SHORT:
+                pair.invested_capital = get_invested_capital(independent_stock_price,
+                                                             dependent_stock_price,
+                                                             pair.long_shares, pair.short_shares,
+                                                             pair.short_entry_price)
+                uninvested_capital += pair.invested_capital
+                pair.invested_capital = pair.long_shares = pair.short_shares = pair.short_entry_price = 0
+            
+            elif signal == Signal.EXIT_LONG_AND_ENTER_SHORT:
+                pair.invested_capital = get_invested_capital(dependent_stock_price,
+                                                             independent_stock_price,
+                                                             pair.long_shares, pair.short_shares,
+                                                             pair.short_entry_price)
+                uninvested_capital += pair.invested_capital
+                
+                investment_sum = PORTFOLIO_PORTION_INVESTED_PER_TRADE * uninvested_capital
+                pair.long_shares, pair.short_shares, pair.short_entry_price = enter_position(investment_sum, dependent_stock_price,
+                                                                          independent_stock_price, pair.hedge_ratio, signal)
+                pair.invested_capital = investment_sum
+                uninvested_capital -= investment_sum
+            
+            else:
+                pair.invested_capital = get_invested_capital(independent_stock_price,
+                                                             dependent_stock_price,
+                                                             pair.long_shares, pair.short_shares,
+                                                             pair.short_entry_price)
+                uninvested_capital += pair.invested_capital
+                
+                investment_sum = PORTFOLIO_PORTION_INVESTED_PER_TRADE * uninvested_capital
+                pair.long_shares, pair.short_shares, pair.short_entry_price = enter_position(investment_sum, dependent_stock_price,
+                                                                          independent_stock_price, pair.hedge_ratio, signal)
+                pair.invested_capital = investment_sum
+                uninvested_capital -= investment_sum
+
+        capital[i] = uninvested_capital + sum(pair.invested_capital for pair in pairs)
+    
+    return capital
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from backtesting import get_capital, get_roi, get_sharpe_ratio
+import sys
+from backtesting import test
 from fetch_data import get_best_spread, get_cointegrated_pairs, get_correlated_pairs, get_hedge_ratios, get_market_data, get_returns, get_z_score
 from graph_data import graph_pair_trades
 from tickers import TICKERS
@@ -12,26 +13,51 @@ def main():
     market_returns = get_returns(market_data)
     correlated_pairs = get_correlated_pairs(market_returns) # this ensures all hedge ratios are positive
     cointegrated_pairs_data = get_cointegrated_pairs(correlated_pairs, market_data)
+    selected_pairs = []
 
     for [[stock1, stock2], pair_data] in cointegrated_pairs_data:
         hedge_ratios = get_hedge_ratios(pair_data[stock1], pair_data[stock2])
         pair_data['Spread'], best_p_value, best_hedge_ratio, dependent_stock, independent_stock = get_best_spread(stock1, stock2, pair_data, hedge_ratios)
+        if best_p_value <= 0.05:
+            selected_pairs.append(Pair(pair_data, dependent_stock, independent_stock, best_hedge_ratio))
+    
+    if not selected_pairs:
+        sys.exit('No stationary spreads found.')
 
-        if best_p_value > 0.05:
-            continue # not stationary
+    for pair in selected_pairs:
+        pair.data['Z-score'] = get_z_score(pair.data['Spread'])
+        pair.data['Position'] = get_positions(pair.data['Z-score'].dropna())
+        pair.data['Signal'] = get_signals(pair.data['Position'])
+    
+    capital = test(selected_pairs)
+    print(capital)
+    """
+        continue # not stationary
 
-        print(f'Spread = {dependent_stock} - β * {independent_stock}, β = {best_hedge_ratio:.4f}')
-        pair_data['Z-score'] = get_z_score(pair_data['Spread'])
-        pair_data['Position'] = get_positions(pair_data['Z-score'].dropna())
-        pair_data['Signal'] = get_signals(pair_data['Position'])
-        pair_data['Capital'], pair_data['Invested'] = get_capital(pair_data[dependent_stock], pair_data[independent_stock],
-                                           pair_data['Position'], pair_data['Signal'], best_hedge_ratio)
-        
-        sharpe_ratio = get_sharpe_ratio(pair_data['Invested'])
-        roi = get_roi(pair_data['Capital'].iloc[-1])
-        
-        print(f'ROI: {roi:.2f}%, Sharpe Ratio: {sharpe_ratio:.4f}\n')
-        graph_pair_trades(stock1, stock2, pair_data)
+    print(f'Spread = {dependent_stock} - β * {independent_stock}, β = {best_hedge_ratio:.4f}')
+    pair_data['Z-score'] = get_z_score(pair_data['Spread'])
+    pair_data['Position'] = get_positions(pair_data['Z-score'].dropna())
+    pair_data['Signal'] = get_signals(pair_data['Position'])
+    pair_data['Capital'], pair_data['Invested'] = get_capital(pair_data[dependent_stock], pair_data[independent_stock],
+                                        pair_data['Position'], pair_data['Signal'], best_hedge_ratio)
+    
+    sharpe_ratio = get_sharpe_ratio(pair_data['Invested'])
+    roi = get_roi(pair_data['Capital'].iloc[-1])
+    
+    print(f'ROI: {roi:.2f}%, Sharpe Ratio: {sharpe_ratio:.4f}\n')
+    graph_pair_trades(stock1, stock2, pair_data)
+    """
+
+class Pair:
+    def __init__(self, data, dependent_stock, independent_stock, hedge_ratio):
+        self.data = data
+        self.dependent_stock = dependent_stock
+        self.independent_stock = independent_stock
+        self.hedge_ratio = hedge_ratio
+        self.invested_capital = 0
+        self.long_shares = 0
+        self.short_shares = 0
+        self.short_entry_price = 0
 
 if __name__ == '__main__':
     main()
