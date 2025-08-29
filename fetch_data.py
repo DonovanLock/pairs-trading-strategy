bc5c8fac707b9883d1f61ecfe46de544fbcd9201
Donovan Lock <donovanlock@gmail.com>
2025-08-30 00:58:14 +0100

    Calculate backtesting spread correctly

--- fetch_data.py
+++ fetch_data.py
@@ -5,24 +5,34 @@ import yfinance as yf
 
 from datetime import datetime, timedelta
 
+from pair import Pair
 from tickers import TICKERS
 
-def get_market_data() -> pd.DataFrame:
-    start_date = datetime.now() - timedelta(days=6*365)
-
-    historic_data = yf.download(tickers=TICKERS, start=start_date, period='3y', auto_adjust=False, progress=False)
+def get_market_data(tickers: list[str], end_date: datetime) -> pd.DataFrame:
+    market_data = yf.download(tickers=tickers, end=end_date, period='3y', auto_adjust=False, progress=False)
 
     failed_tickers = list(yf.shared._ERRORS.keys())
     if failed_tickers:
         sys.exit('Failed to download market data for: ' + str(failed_tickers))
-
-    # for this to work with a vpn, i need to switch country every time it runs?
-    if 'Adj Close' in historic_data.columns:
-        adjusted_data = historic_data['Adj Close']
+    
+    if 'Adj Close' in market_data.columns:
+        adjusted_data = market_data['Adj Close']
     else:
-        adjusted_data = historic_data['Close']
+        adjusted_data = market_data['Close']
 
     return adjusted_data
+
+def get_historic_data() -> pd.DataFrame:
+    end_date = datetime.now() - timedelta(days=3*365)
+    historic_data = get_market_data(TICKERS, end_date)
+
+    return historic_data
+
+def get_backtesting_data(dependent_stock: str, independent_stock: str) -> pd.Series:
+    current_date = datetime.now()
+    backtesting_data = get_market_data([dependent_stock, independent_stock], current_date)
+    
+    return backtesting_data
 
 def get_returns(adjusted_data: pd.DataFrame) -> pd.DataFrame:
     enough_trading_days = adjusted_data.notna().sum() > 700
@@ -33,3 +43,9 @@ def get_returns(adjusted_data: pd.DataFrame) -> pd.DataFrame:
     filtered_returns = returns.loc[:, enough_moving_days]
 
     return filtered_returns
+
+def get_backtesting_spread(pair: Pair) -> pd.Series:
+    backtesting_data = get_backtesting_data(pair.independent_stock, pair.dependent_stock)
+    backtesting_spread = backtesting_data[pair.dependent_stock] - pair.hedge_ratio * backtesting_data[pair.independent_stock]
+
+    return backtesting_spread.replace([np.inf, -np.inf], np.nan).dropna()
