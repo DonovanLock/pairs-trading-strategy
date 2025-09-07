b5bdb10da45a8fb867e36768fdbac5ddd9f2467f
Donovan Lock <donovanlock@gmail.com>
2025-09-07 01:34:26 +0100

    Fixed data timeframe, accounting for the rolling window

--- src/pairs/fetch_data.py
+++ src/pairs/fetch_data.py
@@ -5,11 +5,12 @@ import yfinance as yf
 
 from datetime import datetime, timedelta
 
+from data.config import ROLLING_WINDOW, TRADING_DAYS_RATIO
 from pairs.pair import Pair
 from data.tickers import TICKERS
 
-def get_market_data(tickers: list[str], end_date: datetime) -> pd.DataFrame:
-    market_data = yf.download(tickers=tickers, end=end_date, period='3y', auto_adjust=False, progress=False)
+def get_market_data(tickers: list[str], start: datetime, end: datetime) -> pd.DataFrame:
+    market_data = yf.download(tickers=tickers, start=start, end=end, auto_adjust=False, progress=False)
 
     failed_tickers = list(yf.shared._ERRORS.keys())
     if failed_tickers:
@@ -23,14 +24,16 @@ def get_market_data(tickers: list[str], end_date: datetime) -> pd.DataFrame:
     return adjusted_data
 
 def get_historic_data() -> pd.DataFrame:
-    end_date = datetime.now() - timedelta(days=3*365)
-    historic_data = get_market_data(TICKERS, end_date)
+    start_date = datetime.now() - timedelta(days=4*365+TRADING_DAYS_RATIO*ROLLING_WINDOW)
+    end_date = datetime.now() - timedelta(days=365+TRADING_DAYS_RATIO*ROLLING_WINDOW)
+    historic_data = get_market_data(TICKERS, start_date, end_date)
 
     return historic_data
 
 def get_backtesting_data(dependent_stock: str, independent_stock: str) -> pd.DataFrame:
+    start_date = datetime.now() - timedelta(days=365+TRADING_DAYS_RATIO*ROLLING_WINDOW)
     current_date = datetime.now()
-    backtesting_data = get_market_data([dependent_stock, independent_stock], current_date)
+    backtesting_data = get_market_data([dependent_stock, independent_stock], start_date, current_date)
     
     return backtesting_data
 
